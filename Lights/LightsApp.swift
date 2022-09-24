916d082756613c41bb30900613ea0671a359ba9d
Chris <chris@koch.codes>
2022-09-24 12:29:57 +0200

    Initial Commit

---
+++ Lights/LightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightsApp.swift
+//  Lights
+//
+//  Created by Christopher Koch on 24.09.22.
+//
+
+import SwiftUI
+
+@main
+struct LightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
